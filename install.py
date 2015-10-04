1af90fcebf6e64a175a2315066b2cd266d741b16
Roderick <rsdenijs@lsr.ei.tum.de>
2015-10-04 16:47:56 +0200

    copy for vim also

--- install.py
+++ install.py
@@ -1,7 +1,9 @@
 from subprocess import call
 import os
-out =  "ln -s vim/.vimrc ".split() + [os.path.join(os.path.expanduser('~'),'.nvimrc')]
-call(out)
+nvim =  "ln -s".split()+  os.path.abspath("vim/.vimrc").split() + [os.path.join(os.path.expanduser('~'),'.nvimrc')]
+call(nvim)
+vim =  "ln -s".split()+  os.path.abspath("vim/.vimrc").split() + [os.path.join(os.path.expanduser('~'),'.vimrc')]
+call(vim)
 
 
 
