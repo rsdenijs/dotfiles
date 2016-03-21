9f78ec977d94402ba6265e9a0fedd3716973b5a4
roderick <roderick.denijs@check24.de>
2016-03-21 16:12:29 +0100

    path install update

--- install.py
+++ install.py
@@ -1,6 +1,12 @@
 from subprocess import call
 import os
-nvim =  "ln -s".split()+  os.path.abspath("vim/.vimrc").split() + [os.path.join(os.path.expanduser('~'),'.nvimrc')]
+goal = os.path.join(os.path.expanduser('~'),'.config/nvim')
+try:
+	os.makedirs(goal)
+except Exception as e:
+	print e
+	pass
+nvim =  "ln -s".split()+  os.path.abspath("vim/.vimrc").split() + [os.path.join(goal,'init.vim')]
 call(nvim)
 vim =  "ln -s".split()+  os.path.abspath("vim/.vimrc").split() + [os.path.join(os.path.expanduser('~'),'.vimrc')]
 call(vim)
