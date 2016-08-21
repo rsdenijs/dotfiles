e3b5c2e1cd8afbf6e9df10879520ae469c479dfa
rsdenijs <rsdenijs@gmail.com>
2016-08-21 11:43:57 +0000

    tmux

--- install.py
+++ install.py
@@ -8,8 +8,15 @@ except Exception as e:
 	pass
 nvim =  "ln -s".split()+  os.path.abspath("vim/.vimrc").split() + [os.path.join(goal,'init.vim')]
 call(nvim)
+
 vim =  "ln -s".split()+  os.path.abspath("vim/.vimrc").split() + [os.path.join(os.path.expanduser('~'),'.vimrc')]
 call(vim)
+
+tmux = "ln -s".split() + os.path.abspath("tmux/tmux.conf").split() + [os.path.join(os.path.expanduser('~'),'.tmux.conf')]
+call(tmux)
+
+bashrc = "ln -s".split() + os.path.abspath("bashrc/bashrc.conf").split() + [os.path.join(os.path.expanduser('~'),'.bashrc.conf')]
+call(bashrc)
 
 
 
